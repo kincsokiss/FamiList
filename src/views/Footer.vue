4b9a56e66c5765f0b391cf80c029b0fae0e7ad58
lorinczdorakinga <lorinczdora000@gmail.com>
2023-06-13 15:58:55 +0300

    FL-9 Create The Header And Footer Component

---
+++ src/views/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <ion-footer>
+      <ion-toolbar>
+        <ion-title> Footer </ion-title>
+      </ion-toolbar>
+    </ion-footer>
+  </template>
+  
+  <script>
+  export default({
+   name:'FooterVue',
+  });
+ </script>
