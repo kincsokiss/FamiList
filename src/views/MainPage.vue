8402116024dd6e88419d9a9e1eb50a608df7128e
Viki <pvikit7@gmail.com>
2023-12-13 15:57:53 +0200

    FL-58 Refresh Button and Text Modified

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,20 +1,30 @@
 <template>
   <body class="main">
     <ion-content>
-      <ion-grid >
-      <ion-row>
-        <ion-col>
-          <CalendarItem/>
-        </ion-col>
-      </ion-row>
-      <ion-row>
-        <ion-col>
-          <ToDoList/>
-        </ion-col>
-      </ion-row>
-    </ion-grid>
+      <ion-refresher slot="fixed" :pull-factor="0.5" :pull-min="100" :pull-max="200" @ionRefresh="handleRefresh($event)">
+          <ion-refresher-content
+            :pulling-icon="chevronDownCircleOutline"
+            refreshing-spinner="circles"
+            refreshing-text="Refreshing..."
+          >
+          </ion-refresher-content>
+      </ion-refresher>
+      <div>
+        <ion-grid >
+          <ion-row>
+            <ion-col>
+              <CalendarItem/>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <ToDoList :isRefreshing="isRefreshing" @updateRefreshingState="updateRefreshingState"/>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
+        
     </ion-content>
-    
   </body>
     
 </template>
@@ -22,7 +32,19 @@
 <script setup>
 import ToDoList from './ToDoList.vue';
 import CalendarItem from './CalendarItem.vue';
-import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/vue';
+import { IonGrid, IonRow, IonCol, IonContent, IonRefresher, IonRefresherContent } from '@ionic/vue';
+import { chevronDownCircleOutline } from 'ionicons/icons';
+import { ref } from 'vue';
+
+const isRefreshing = ref(false);
+const handleRefresh = async (event) => {
+  event.detail.complete();
+  isRefreshing.value = true;
+}
+
+function updateRefreshingState(state){
+  isRefreshing.value = state;
+}
 </script>
 
 <style lang="scss" scoped>
@@ -33,5 +55,9 @@ import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/vue';
   .main {
     max-height: 100%;
     // background-color: #f7d6c5;
+  }
+
+  ion-refresher {
+    margin-top: 2.5rem;
   }
 </style>
